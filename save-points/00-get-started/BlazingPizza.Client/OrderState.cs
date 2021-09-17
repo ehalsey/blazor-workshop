b99d98f27a95235d1a0036ea2671878eefeebe82
ehalsey <ehalsey@Win10-DEV>
2021-09-17 16:10:49 +0000

    Authentication

--- save-points/00-get-started/BlazingPizza.Client/OrderState.cs
+++ save-points/00-get-started/BlazingPizza.Client/OrderState.cs
@@ -51,6 +51,10 @@ namespace BlazingPizza.Client
             Order.Pizzas.Remove(pizza);
         }
 
+        public void ReplaceOrder(Order order)
+        {
+            Order = order;
+        }
     }
 
 }
--- save-points/00-get-started/BlazingPizza.Client/Program.cs
+++ save-points/00-get-started/BlazingPizza.Client/Program.cs
@@ -18,7 +18,12 @@ namespace BlazingPizza.Client
             builder.Services.AddScoped<OrderState>();
 
             // Add auth services
-            builder.Services.AddApiAuthorization();
+            builder.Services.AddApiAuthorization<PizzaAuthenticationState>(options =>
+            {
+                options.AuthenticationPaths.LogOutSucceededPath = "";
+            });
+
+
             //efh - need to understnad this
             builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
