72bb1265ff2e5afbb40aa43fd838a2a9c94d1a95
ehalsey <ehalsey@Win10-DEV>
2021-09-17 04:39:24 +0000

    Checkout with Validation

--- save-points/00-get-started/BlazingPizza.Shared/Address.cs
+++ save-points/00-get-started/BlazingPizza.Shared/Address.cs
@@ -6,16 +6,20 @@ namespace BlazingPizza
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Line1 { get; set; }
 
         public string Line2 { get; set; }
 
+        [Required(ErrorMessage = "How do you expect to receive the pizza if we don't even know what city you're in?"), MaxLength(50)]
         public string City { get; set; }
 
         public string Region { get; set; }
 
+        [Required]
         public string PostalCode { get; set; }
     }
 }
